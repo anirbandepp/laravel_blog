38e7d4b0cfdebcceeeb2554a3f121375f3aec84a
anirbandepp <anirbanshow.dey12@gmail.com>
2022-04-26 00:54:00 +0530

    collection splice

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -5,54 +5,47 @@
         <div class="container">
             <div class="row align-items-stretch retro-layout-2">
                 <div class="col-md-4">
-                    <a href="single.html" class="h-entry mb-30 v-height gradient"
-                        style="background-image: url('{{ asset('website') }}/images/img_1.jpg');">
+                    @foreach ($postFirst2 as $item)
+                        <a href="{{ route('post', ['slug' => $item->slug]) }}" class="h-entry mb-30 v-height gradient"
+                            style="background-image: url('{{ $item->image }}');">
 
-                        <div class="text">
-                            <h2>The AI magically removes moving objects from videos.</h2>
-                            <span class="date">July 19, 2019</span>
-                        </div>
-                    </a>
-                    <a href="single.html" class="h-entry v-height gradient"
-                        style="background-image: url('{{ asset('website') }}/images/img_2.jpg');">
-
-                        <div class="text">
-                            <h2>The AI magically removes moving objects from videos.</h2>
-                            <span class="date">July 19, 2019</span>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-4">
-                    <a href="single.html" class="h-entry img-5 h-100 gradient"
-                        style="background-image: url('{{ asset('website') }}/images/img_v_1.jpg');">
-
-                        <div class="text">
-                            <div class="post-categories mb-3">
-                                <span class="post-category bg-danger">Travel</span>
-                                <span class="post-category bg-primary">Food</span>
+                            <div class="text">
+                                <h2> {{ $item->title }} </h2>
+                                <span class="date"> {{ $item->created_at->format('M d, Y') }} </span>
                             </div>
-                            <h2>The AI magically removes moving objects from videos.</h2>
-                            <span class="date">July 19, 2019</span>
-                        </div>
-                    </a>
+                        </a>
+                    @endforeach
                 </div>
                 <div class="col-md-4">
-                    <a href="single.html" class="h-entry mb-30 v-height gradient"
-                        style="background-image: url('{{ asset('website') }}/images/img_3.jpg');">
+                    @foreach ($postMiddle1 as $item)
+                        <a href="{{ route('post', ['slug' => $item->slug]) }}" class="h-entry img-5 h-100 gradient"
+                            style="background-image: url('{{ $item->image }}');">
 
-                        <div class="text">
-                            <h2>The 20 Biggest Fintech Companies In America 2019</h2>
-                            <span class="date">July 19, 2019</span>
-                        </div>
-                    </a>
-                    <a href="single.html" class="h-entry v-height gradient"
-                        style="background-image: url('{{ asset('website') }}/images/img_4.jpg');">
+                            <div class="text">
+                                <div class="post-categories mb-3">
+                                    @foreach ($item->tags as $tag)
+                                        <span class="post-category bg-danger">
+                                            {{ $tag->name }}
+                                        </span>
+                                    @endforeach
+                                </div>
+                                <h2> {{ $item->title }} </h2>
+                                <span class="date"> {{ $item->created_at->format('M d, Y') }} </span>
+                            </div>
+                        </a>
+                    @endforeach
+                </div>
+                <div class="col-md-4">
+                    @foreach ($postLast2 as $item)
+                        <a href="{{ route('post', ['slug' => $item->slug]) }}" class="h-entry mb-30 v-height gradient"
+                            style="background-image: url('{{ $item->image }}');">
 
-                        <div class="text">
-                            <h2>The 20 Biggest Fintech Companies In America 2019</h2>
-                            <span class="date">July 19, 2019</span>
-                        </div>
-                    </a>
+                            <div class="text">
+                                <h2> {{ $item->title }} </h2>
+                                <span class="date"> {{ $item->created_at->format('M d, Y') }} </span>
+                            </div>
+                        </a>
+                    @endforeach
                 </div>
             </div>
         </div>
