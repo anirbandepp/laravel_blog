ee1f2a42645f6e0dcbb76ab95c062f32f76254d4
anirbandepp <anirbanshow.dey12@gmail.com>
2022-04-26 00:58:22 +0530

    check

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -11,19 +11,18 @@ class FrontEndController extends Controller
     {
         $posts = Post::with('category', 'user', 'tags')->orderBy('created_at', 'DESC')->take(5)->get();
         $postFirst2 = $posts->splice(0, 2);
-        dd($posts);
-        // $postMiddle1 = $posts->splice(0, 1);
-        // $postLast2 = $posts->splice(0);
+        $postMiddle1 = $posts->splice(0, 1);
+        $postLast2 = $posts->splice(0);
 
-        // $recentPosts =  Post::with('category', 'user', 'tags')->orderBy('created_at', 'DESC')->paginate(6);
+        $recentPosts =  Post::with('category', 'user', 'tags')->orderBy('created_at', 'DESC')->paginate(6);
 
-        // return view('website.home', [
-        //     'posts' => $posts,
-        //     'recentPosts' => $recentPosts,
-        //     'postFirst2' => $postFirst2,
-        //     'postMiddle1' => $postMiddle1,
-        //     'postLast2' => $postLast2,
-        // ]);
+        return view('website.home', [
+            'posts' => $posts,
+            'recentPosts' => $recentPosts,
+            'postFirst2' => $postFirst2,
+            'postMiddle1' => $postMiddle1,
+            'postLast2' => $postLast2,
+        ]);
     }
 
     public function about()
