ea278912bbc77bb40d4ec17e899c38ba49a41739
anirbandepp <anirbanshow.dey12@gmail.com>
2022-05-10 23:49:22 +0530

    blog

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -78,6 +78,7 @@
 
         @yield('content')
 
+        {{-- footer start --}}
         <div class="site-footer">
             <div class="container">
                 <div class="row mb-5">
@@ -117,23 +118,9 @@
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-12 text-center">
-                        <p>
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                            Copyright &copy;
-                            <script>
-                                document.write(new Date().getFullYear());
-                            </script> All rights reserved | This template is made with <i
-                                class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com"
-                                target="_blank">Colorlib---</a>Downloaded from <a href="https://themeslab.org/"
-                                target="_blank">Themeslab</a>
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        </p>
-                    </div>
-                </div>
             </div>
         </div>
+        {{-- footer end --}}
 
     </div>
 
